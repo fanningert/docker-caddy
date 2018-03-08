076d627c33a6f0695757691f500812e972347b20
Thomas Fanninger <thomas@fanninger.at>
2018-03-08 20:08:06 +0100

    new version

--- root/etc/cont-init.d/90_configure_caddy.sh
+++ root/etc/cont-init.d/90_configure_caddy.sh
@@ -5,4 +5,6 @@ mkdir -p /config
 dockerize -template /app/start.sh:/config/start.sh
 dockerize -no-overwrite -template /app/Caddyfile:/config/Caddyfile
 
+chmod +x /config/start.sh
+
 chown -R app:users /config
