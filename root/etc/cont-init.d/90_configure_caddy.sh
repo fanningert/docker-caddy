0601720186905b56bac059c5c86d4bd24e743c7d
Thomas Fanninger <thomas@fanninger.at>
2018-03-19 20:33:49 +0100

    Bugfix Cron

--- root/etc/cont-init.d/90_configure_caddy.sh
+++ root/etc/cont-init.d/90_configure_caddy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/with-contenv bash
 
 mkdir -p /config/cert
+mkdir -p /conf
 
-dockerize -template /app/start_caddy.sh:/config/start_caddy.sh
+dockerize -template /app/start_caddy.sh:/conf/start_caddy.sh
 dockerize -no-overwrite -template /app/Caddyfile:/config/Caddyfile
 
-chmod +x /config/start_caddy.sh
+chmod +x /conf/start_caddy.sh
 
+chown -R app:users /conf
 chown -R app:users /config
 chown -R app:users /srv
